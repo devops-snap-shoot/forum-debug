f64569ddd5f42a17ef0e3ff932c604f098be032a
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-30 11:53:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
+import tiktoken
 
-html='<iframe src="https://www.w3schools.com" title="W3Schools Free Online Web Tutorials"></iframe>'
-
-st.components.v1.html(html, width=None, height=None, scrolling=False)
+st.write("Hello world")
